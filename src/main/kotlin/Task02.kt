3446b9355ec1083ff66688649149bb7bb318a9c8
ivzb <izahariev96@gmail.com>
2022-12-02 11:36:11 +0200

    simplified task 2

--- src/main/kotlin/Task02.kt
+++ src/main/kotlin/Task02.kt
@@ -1,71 +1,53 @@
-import Task02.Shape.*
-
 object Task02 {
 
-    enum class Shape(val score: Int) {
-        Rock(1),
-        Paper(2),
-        Scissors(3)
+    fun partOne() = parseInput().sumOf {
+        val other = it[0] - 'A'
+        val me = it[2] - 'X'
+
+        val outcome = when {
+            other == me -> DRAW
+            (me + 1) % 3 == other % 3 -> LOSE
+            else -> WIN
+        } * 3
+        val shape = me + 1
+
+        outcome + shape
     }
 
-    enum class Outcome(val score: Int) {
-        Lose(0),
-        Draw(3),
-        Win(6)
+    fun partTwo() = parseInput().sumOf {
+        val other = it[0] - 'A' + 1
+        val me = it[2] - 'X'
+
+        val outcome = me * 3
+        val shape = when (me) {
+            LOSE -> loseTo[other]
+            WIN -> winTo[other]
+            DRAW -> other
+            else -> 0
+        } ?: 0
+
+        outcome + shape
     }
 
-    val shapes = mapOf(
-        "A" to Rock,
-        "B" to Paper,
-        "C" to Scissors,
-        "X" to Rock,
-        "Y" to Paper,
-        "Z" to Scissors,
+    private fun parseInput() = readInput("02").split("\n")
+
+    private const val LOSE = 0
+    private const val DRAW = 1
+    private const val WIN = 2
+
+    private const val ROCK = 1
+    private const val PAPER = 2
+    private const val SCISSORS = 3
+
+    private val winTo = mapOf(
+        ROCK to PAPER,
+        SCISSORS to ROCK,
+        PAPER to SCISSORS,
     )
 
-    val need = mapOf(
-        "X" to Outcome.Lose,
-        "Y" to Outcome.Draw,
-        "Z" to Outcome.Win,
+    private val loseTo = mapOf(
+        ROCK to SCISSORS,
+        SCISSORS to PAPER,
+        PAPER to ROCK,
     )
-
-    val outcomes = mapOf(
-        Pair(Rock, Scissors) to Outcome.Lose,
-        Pair(Scissors, Rock) to Outcome.Win,
-
-        Pair(Scissors, Paper) to Outcome.Lose,
-        Pair(Paper, Scissors) to Outcome.Win,
-
-        Pair(Paper, Rock) to Outcome.Lose,
-        Pair(Rock, Paper) to Outcome.Win,
-
-        Pair(Rock, Rock) to Outcome.Draw,
-        Pair(Paper, Paper) to Outcome.Draw,
-        Pair(Scissors, Scissors) to Outcome.Draw,
-    )
-
-    // 13446
-    fun partOne() = parseInput()
-        .map { (opponent, me) ->
-            val it = shapes[opponent] to shapes[me]
-            val outcome = outcomes[it]
-            (outcome?.score ?: 0) + (it.second?.score ?: 0)
-        }
-        .sum()
-
-    // 13509
-    fun partTwo() =
-        parseInput()
-            .map { (opponent, me) ->
-                val n = need[me]
-                val entry = outcomes.entries.find { it.value == n && it.key.first == shapes[opponent] }
-                val outcome = entry?.value
-                val shape = entry?.key?.second
-                (outcome?.score ?: 0) + (shape?.score ?: 0)
-            }
-            .sum()
-
-    private fun parseInput() = readInput("02")
-        .split("\n")
-        .map { it.split(' ') }
 }
