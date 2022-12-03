fde4cbe30795b29aa44c923fcd8515258f10c696
ivzb <izahariev96@gmail.com>
2022-12-03 15:43:22 +0200

    2021/01

--- src/main/kotlin/_2021/Task01.kt
+++ src/main/kotlin/_2021/Task01.kt
@@ -4,36 +4,14 @@ import readInput
 
 object Task01 {
 
-    private data class Measurement(val value: Int, val level: Level)
-
-    private enum class Level {
-        NA,
-        DECREASED,
-        INCREASED,
-        NO_CHANGE
-    }
-
     fun partOne() = parseInput()
-        .countIncreasedLevel()
+        .zipWithNext()
+        .count { it.first < it.second }
 
     fun partTwo() = parseInput()
         .windowed(size = 3, step = 1) { it.sum() }
-        .countIncreasedLevel()
-
-    private fun List<Int>.countIncreasedLevel(): Int =
-        fold(ArrayList<Measurement>()) { measurements, it ->
-            val last = measurements.lastOrNull()
-            val level = when {
-                last == null -> Level.NA
-                it > last.value -> Level.INCREASED
-                it < last.value -> Level.DECREASED
-                else -> Level.NO_CHANGE
-            }
-
-            measurements.add(Measurement(it, level))
-            measurements
-        }
-        .count { it.level == Level.INCREASED }
+        .zipWithNext()
+        .count { it.first < it.second }
 
     private fun parseInput() = readInput("_2021/01")
         .split("\n")
