d16a8f71510d760fca0e3713edfb93167e0c774e
ivzb <izahariev96@gmail.com>
2022-12-04 07:35:03 +0200

    2022/04

--- src/main/kotlin/_2022/Task04.kt
+++ src/main/kotlin/_2022/Task04.kt
@@ -4,19 +4,23 @@ import readInput
 
 object Task04 {
 
-    fun partOne() = parseInput().map {
+    // 466
+    fun partOne() = parseInput()
+        .map { (left, right) -> left.containsAll(right) || right.containsAll(left) }
+        .count { it }
 
-        val b = 5
-        ""
-    }
-
-    fun partTwo() = parseInput().map {
-
-        val b = 5
-        ""
-    }
+    // 865
+    fun partTwo() = parseInput()
+        .map { (left, right) -> left.intersect(right).isNotEmpty() }
+        .count { it }
 
     private fun parseInput() = readInput("_2022/04")
         .split("\n")
+        .map {
+            it
+                .split(",")
+                .map { it.split('-').map { it.toInt() } }
+                .map { (from, to) -> (from..to).toSet() }
+        }
 
 }
