b828994cc0ec31133a1d872a5cd84722c61770b0
ivzb <izahariev96@gmail.com>
2022-12-05 11:21:31 +0200

    2022/05

--- src/main/kotlin/_2022/Task05.kt
+++ src/main/kotlin/_2022/Task05.kt
@@ -5,66 +5,50 @@ import java.util.*
 
 object Task05 {
 
-    // GRTSWNJHH
-    fun partOne() = parseInput()
-        .let { (stacks, instructions) ->
-            instructions.forEach { (move, from, to) ->
-                repeat(move) {
-                    stacks[to]?.addFirst(stacks[from]?.removeFirst())
-                }
-            }
-
-            topCrates(stacks)
+    fun partOne() = parseInput().let { (stacks, instructions) ->
+        instructions.forEach { (move, from, to) ->
+            repeat(move) { stacks[to]?.push(stacks[from]?.pop()) }
         }
 
-    // QLFQDBBHM
-    fun partTwo() = parseInput()
-        .let { (stacks, instructions) ->
-            instructions.forEach { (move, from, to) ->
-                val temp = LinkedList<String>()
-                repeat(move) { temp.addFirst(stacks[from]?.removeFirst()) }
-                repeat(move) { stacks[to]?.addFirst(temp.removeFirst()) }
-            }
+        findTopCrates(stacks)
+    }
 
-            topCrates(stacks)
+    fun partTwo() = parseInput().let { (stacks, instructions) ->
+        instructions.forEach { (move, from, to) ->
+            val temp = LinkedList<String>()
+            repeat(move) { temp.push(stacks[from]?.pop()) }
+            repeat(move) { stacks[to]?.push(temp.pop()) }
         }
 
-    private fun topCrates(stacks: Map<Int, LinkedList<String>>): String =
-        stacks.entries.sortedBy { it.key }.joinToString("") { it.value.peekFirst() }
+        findTopCrates(stacks)
+    }
+
+    private fun findTopCrates(stacks: Map<Int, LinkedList<String>>): String =
+        stacks.entries.sortedBy { it.key }.joinToString("") { it.value.peek() }
 
     private fun parseInput() = readInput("_2022/05")
         .split("\n\n")
         .let { (stacks, instructions) -> parseStacks(stacks) to parseInstructions(instructions) }
 
-    // todo: Change Int? to Int
-    private fun parseStacks(input: String): Map<Int, LinkedList<String>> {
-        return input.split('\n').let {
-            val positions = it
-                .last()
-                .mapIndexed { index, char -> index to char }
-                .filter { (_, char) -> char.isDigit() }
-                    // todo: improve
-                .associate { (index, char) -> index to char.toString().toInt() }
-
+    private fun parseStacks(input: String): Map<Int, LinkedList<String>> = input
+        .split('\n')
+        .map {
             it
-                .map { line ->
-                    line
-                        .mapIndexed { index, char ->
-                            (positions[index] ?: -1) to char
-                        }
-                        .filter { (index, char) -> char.isLetter() }
+                .chunked(4)
+                .mapIndexed { index, it ->
+                    val position = index + 1
+                    val (_, crane) = it.toCharArray()
+                    position to crane
                 }
-                .flatten()
-                .groupBy { (index) -> index }
-                .map { (index, list) -> index to LinkedList(list.map { (_, char) -> char.toString() }) }
-                .toMap()
+                .filter { (_, char) -> char.isLetter() }
         }
-    }
+        .flatten()
+        .groupBy { (index) -> index }
+        .map { (index, list) -> index to LinkedList(list.map { (_, char) -> char.toString() }) }
+        .toList()
+        .toMap()
 
     private fun parseInstructions(input: String): List<List<Int>> = input
         .split('\n')
-        .map {
-            it.split(' ').filter { it.toIntOrNull() != null }.map { it.toInt() }
-        }
-
+        .map { it.split(' ').mapNotNull { it.toIntOrNull() } }
 }
