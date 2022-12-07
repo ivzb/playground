83c00a247c3d143538fec666d513ac30a0d45bde
ivzb <izahariev96@gmail.com>
2022-12-07 08:13:37 +0200

    2022/07

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,15 +1,15 @@
-import _2022.Task06
+import _2022.Task07
 
 fun main() {
-    val task = Task06
+    val task = Task07
 
     task.partOne().let {
-        val result = if (it.toString() == "1779") "SUCCESS" else "FAILURE"
+        val result = if (it.toString() == "2104783") "SUCCESS" else "FAILURE"
         println("$result - $it")
     }
 
     task.partTwo().let {
-        val result = if (it.toString() == "2635") "SUCCESS" else "FAILURE"
+        val result = if (it.toString() == "5883165") "SUCCESS" else "FAILURE"
         println("$result - $it")
     }
 }
---
+++ src/main/kotlin/_2022/Task07.kt
@@ -0,0 +1,73 @@
+package _2022
+
+import readInput
+
+object Task07 {
+
+    private const val MAX_SPACE = 100_000
+    private const val NEEDED_SPACE = 30_000_000
+    private const val TOTAL_SPACE = 70_000_000
+
+    fun partOne() = buildFilesystem().root.flatten()
+        .map { it.space() }
+        .filter { space -> space <= MAX_SPACE }
+        .sum()
+
+    fun partTwo() = buildFilesystem().let { fs ->
+        fs.root.flatten()
+            .map { it.space() }
+            .sorted()
+            .first { space ->
+                space >= NEEDED_SPACE - (TOTAL_SPACE - fs.root.space())
+            }
+    }
+
+    private fun buildFilesystem() =
+        parseInput().fold(Filesystem()) { fs, input -> fs.parse(input) }
+
+    data class Filesystem(val root: Directory, var current: Directory?) {
+        constructor() : this(root = Directory("/"))
+        constructor(root: Directory) : this(root = root, current = root)
+
+        fun parse(input: List<String>): Filesystem =
+            input.let { (arg1, arg2, arg3) ->
+                when (arg1) {
+                    "$" -> current = when (arg3) {
+                        null -> current
+                        "/" -> root
+                        ".." -> current?.parent
+                        else -> current?.children?.find { it.name == arg3 }
+                    }
+
+                    "dir" -> current?.children?.add(
+                        Directory(name = arg2, parent = current)
+                    )
+
+                    else -> current?.files?.add(arg1.toInt())
+                }
+
+                return this
+            }
+    }
+
+    data class Directory(
+        val name: String,
+        val parent: Directory? = null,
+        val children: ArrayList<Directory> = ArrayList(),
+        val files: ArrayList<Int> = ArrayList()
+    ) {
+
+        fun space(): Int =
+            files.sumOf { it } + children.sumOf { it.space() }
+
+        fun flatten(): List<Directory> =
+            children + children.flatMap { it.flatten() }
+    }
+
+    private fun parseInput() = readInput("_2022/07")
+        .split('\n')
+        .map { it.split(" ") }
+
+    operator fun List<String>.component3(): String? = getOrNull(2)
+
+}
