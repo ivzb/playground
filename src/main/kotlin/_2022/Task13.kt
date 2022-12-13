30dc6dd25d604a2fa86a674a119aa7aa0e5a4d84
ivzb <izahariev96@gmail.com>
2022-12-13 10:51:25 +0200

    2022/12

--- src/main/kotlin/_2022/Task13.kt
+++ src/main/kotlin/_2022/Task13.kt
@@ -3,27 +3,25 @@ package _2022
 import Task
 import readInput
 import java.util.*
-import kotlin.Comparator
 import kotlin.collections.ArrayList
 
 object Task13 : Task {
 
     private const val ADDITIONAL_PACKET_1 = "[[2]]"
     private const val ADDITIONAL_PACKET_2 = "[[6]]"
+    private val ADDITIONAL_PACKETS = listOf(ADDITIONAL_PACKET_1, ADDITIONAL_PACKET_2)
 
     override fun partA() = parseInput()
         .mapIndexed { index, (left, right) ->
-            val inTheRightOrder = isInRightOrder(left, right)
-
-            inTheRightOrder to index + 1
+            index to isInRightOrder(left, right)
         }
-        .filter { (inTheRightOrder) -> inTheRightOrder }
-        .sumOf { (_, index) -> index }
+        .filter { (_, inTheRightOrder) -> inTheRightOrder }
+        .sumOf { (index) -> index + 1 }
 
     override fun partB() = parseInput()
         .flatten()
-        .plus(listOf("[[2]]", "[[6]]"))
-        .sortedWith { left, right -> compareTrees(left, right) }
+        .plus(ADDITIONAL_PACKETS)
+        .sortedWith(this::compareTrees)
         .reversed()
         .let {
             val index1 = it.indexOf(ADDITIONAL_PACKET_1) + 1
@@ -82,81 +80,53 @@ object Task13 : Task {
 
     fun isInRightOrder(left: String, right: String): Boolean {
         val cmp = compareTrees(left, right)
-
-        return when (cmp) {
-            1 -> true
-            -1 -> false
-            0 -> error("0 -> they are the same?")
-             else -> error("other value -> $cmp")
+        return when {
+            cmp > 0 -> true
+            cmp < 0 -> false
+            else -> error("they are the same")
         }
     }
 
-    fun compareTrees(left: String, right: String): Int {
+    private fun compareTrees(left: String, right: String): Int {
         val leftTree = parseTree(left)
         val rightTree = parseTree(right)
-
-        return compare(leftTree, rightTree)
+        return compareTrees(leftTree, rightTree)
     }
 
-    private fun compare(left: TreeList, right: TreeList): Int {
-        println(" - Compare $left vs $right")
-
+    private fun compareTrees(left: TreeList, right: TreeList): Int {
         if (left.item != null && right.item != null) {
-            val delta = left.item - right.item
+            return (right.item - left.item).coerceIn(-1, 1)
+        }
 
-            if (left.item < right.item) {
-                println(" - Left side is smaller, so inputs are in the right order")
-                return 1
-            } else if (left.item > right.item) {
-                println(" - Right side is smaller, so inputs are not in the right order")
-                return -1
-            } else {
-                // continue checking
-                return 0
+        if (left.item != null) {
+            val leftChild = TreeList(parent = left.parent)
+            leftChild.add(TreeList(left.item, parent = leftChild))
+            return compareTrees(leftChild, right)
+        }
+
+        if (right.item != null) {
+            val rightChild = TreeList(parent = right.parent)
+            rightChild.add(TreeList(right.item, parent = rightChild))
+            return compareTrees(left, rightChild)
+        }
+
+        val leftStack = LinkedList(left.children)
+        val rightStack = LinkedList(right.children)
+
+        while (leftStack.isNotEmpty() || rightStack.isNotEmpty()) {
+            if (leftStack.isEmpty()) return 1
+            if (rightStack.isEmpty()) return -1
+
+            val leftChild = leftStack.removeFirst()
+            val rightChild = rightStack.removeFirst()
+
+            val cmp = compareTrees(leftChild, rightChild)
+
+            if (cmp != 0) {
+                return cmp
             }
         }
 
-        if (left.item == null && right.item == null) {
-            val leftStack = LinkedList(left.children)
-            val rightStack = LinkedList(right.children)
-
-            while (leftStack.isNotEmpty() || rightStack.isNotEmpty()) {
-                if (leftStack.isEmpty()) {
-                    println(" - Left side ran out of items, so inputs are in the right order")
-                    return 1
-                }
-
-                if (rightStack.isEmpty()) {
-                    println(" - Right side ran out of items, so inputs are not in the right order")
-                    return -1
-                }
-
-                val leftChild = leftStack.removeFirst()
-                val rightChild = rightStack.removeFirst()
-
-                val cmp = compare(leftChild, rightChild)
-
-                if (cmp != 0) {
-                    return cmp
-                }
-            }
-        }
-
-        if (left.item != null && right.item == null) {
-            // create new left tree with list of item instead of item
-            val newLeft = TreeList(parent = left.parent)
-            newLeft.add(TreeList(left.item, parent = newLeft))
-            return compare(newLeft, right)
-        }
-
-        if (left.item == null && right.item != null) {
-            // create new right tree with list of item instead of item
-            val newRight = TreeList(parent = right.parent)
-            newRight.add(TreeList(right.item, parent = newRight))
-            return compare(left, newRight)
-        }
-
-//        return left.children.size - right.children.size
         return 0
     }
 
@@ -169,17 +139,13 @@ object Task13 : Task {
         }
 
         override fun toString(): String {
-            if (item != null) {
-                return item.toString()
-            }
+            if (item != null) return item.toString()
 
-            val ch = children.joinToString(",")
-
-            return if (parent != null) {
-                return "[$ch]"
-            } else {
-                ch
-            }
+            return children
+                .joinToString(",")
+                .let {
+                    if (parent != null) "[$it]" else it
+                }
         }
     }
 
