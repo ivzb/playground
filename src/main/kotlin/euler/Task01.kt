16fe27a8fb135fe3c7bc1fd40914e8a758207db2
ivzb <izahariev96@gmail.com>
2022-12-26 13:42:36 +0200

    euler Multiples of 3 or 5

--- src/main/kotlin/euler/Task01.kt
+++ src/main/kotlin/euler/Task01.kt
@@ -6,16 +6,8 @@ object Task01 : EulerTask {
 
     override val name: String = "Multiples of 3 or 5"
 
-    override fun solution(): Any {
-        var sum = 0
-
-        for (n in 1 until 1000) {
-            if (n % 3 == 0 || n % 5 == 0) {
-                sum += n
-            }
-        }
-
-        return sum
-    }
-
+    override fun solution(): Int =
+        (1 until 1000)
+            .filter { n -> n % 3 == 0 || n % 5 == 0 }
+            .sum()
 }
