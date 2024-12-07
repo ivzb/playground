04329e88ffe6eb03ee8a94a04b43f37297f08124
ivzb <izahariev96@gmail.com>
2024-12-07 10:30:52 +0200

    2024/07

--- src/main/kotlin/advent_of_code/_2024/Task07.kt
+++ src/main/kotlin/advent_of_code/_2024/Task07.kt
@@ -2,85 +2,31 @@ package advent_of_code._2024
 
 import Task
 import readInput
-import java.util.*
 
 object Task07 : Task {
 
-    override fun partA() = parseInput()
-        .let {
-            solve(it, "+*")
+    override fun partA() = solve(withConcat = false)
+
+    override fun partB() = solve(withConcat = true)
+
+    private fun solve(withConcat: Boolean): Long = parseInput()
+        .filter { (targetValue, numbers) ->
+            solve(numbers, index = 1, result = numbers[0], targetValue, withConcat)
+        }
+        .sumOf { (testValue) -> testValue }
+
+    private fun solve(numbers: List<Long>, index: Int, result: Long, targetValue: Long, withConcat: Boolean): Boolean {
+        if (numbers.size == index) {
+            return result == targetValue
         }
 
-    override fun partB() = parseInput()
-        .let {
-            solve(it, "+*|")
+        if (result > targetValue) {
+            return false
         }
 
-    private fun solve(it: List<Pair<Long, List<Long>>>, testOperators: String): Long {
-        val solutions = mutableListOf<Long>()
-        val cachedOperators = cachedOperators(it, testOperators)
-
-        it.forEach { (testValue, remainingNumbers) ->
-            for (operators in cachedOperators[remainingNumbers.size]!!) {
-                val remainingNumbersQ = LinkedList(remainingNumbers)
-                val operatorsQ = LinkedList(operators)
-
-                while (operatorsQ.isNotEmpty()) {
-                    val left = remainingNumbersQ.pop()
-                    val right = remainingNumbersQ.pop()
-                    val operator = operatorsQ.pop()
-
-                    val result = when (operator) {
-                        '+' -> left + right
-                        '*' -> left * right
-                        '|' -> "$left$right".toLong()
-                        else -> throw Exception("unknown operator $operator")
-                    }
-
-                    remainingNumbersQ.push(result)
-
-                    if (result > testValue) {
-                        break
-                    }
-                }
-
-                if (testValue == remainingNumbersQ.pop()) {
-                    solutions.add(testValue)
-                    break
-                }
-            }
-        }
-
-        return solutions.sum()
-    }
-
-    private fun cachedOperators(it: List<Pair<Long, List<Long>>>, testOperators: String): Map<Int, List<List<Char>>> {
-        val testOps = testOperators.map { listOf(it) }
-        val opsMap = mutableMapOf<Int, List<List<Char>>>()
-        val keys = it.groupBy { (_, list) -> list.size }.keys
-
-        keys.forEach { remainingNumbers ->
-            val result = mutableListOf<List<Char>>()
-            val q = LinkedList<List<Char>>()
-            q.addAll(testOps)
-
-            while (q.isNotEmpty()) {
-                val operators = q.pop()
-
-                if (operators.size == remainingNumbers - 1) {
-                    result.add(operators)
-                    continue
-                }
-
-                testOps.forEach {
-                    q.add(operators + it)
-                }
-            }
-
-            opsMap[remainingNumbers] = result
-        }
-
-        return opsMap
+        return solve(numbers, index + 1, result + numbers[index], targetValue, withConcat) ||
+                solve(numbers, index + 1, result * numbers[index], targetValue, withConcat) ||
+                (withConcat && solve(numbers, index + 1, "$result${numbers[index]}".toLong(), targetValue, withConcat))
     }
 
     private fun parseInput() = readInput("_2024/07")
